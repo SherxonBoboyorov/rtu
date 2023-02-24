3e40b4030b73e4e6c0780c64f7dd496bc6bb9f46
SherxonBoboyorov <sherxonboboyorov@gmail.com>
2023-02-24 13:04:30 +0500

    uzgarishlar

---
+++ app/Models/Connect.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Connect extends Model
+{
+    use HasFactory;
+
+    protected $table = 'connects';
+
+    protected $fillable = [
+        'fullname',
+        'phone_number',
+        'comment'
+    ];
+}
